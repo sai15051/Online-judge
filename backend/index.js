4cf6a9827c9c7c481c8f5a4e4996be0548dfe345
sai15051 <sumanthkoka@gmail.com>
2025-01-11 22:15:43 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(
     cors({
-        origin: ["http://localhost:5173",'https://online-judge-fj7y.vercel.app'],
+        origin: ["https://online-judge-fj7y.vercel.app"],
         methods: ["GET", "POST", "PUT", "DELETE"],
         credentials: true,
     })
