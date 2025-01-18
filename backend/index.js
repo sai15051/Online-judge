b9268016a71f6c05d5ec35c86c5a8f46888e475e
sai15051 <sumanthkoka@gmail.com>
2025-01-18 12:42:30 +0530

    cors updates

--- backend/index.js
+++ backend/index.js
@@ -13,12 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
-const allowedOrigins = ["https://online-judge-fj7y.vercel.app"];
+const allowedOrigins = [
+    "https://online-judge-fj7y.vercel.app",
+    "https://online-judge-fj7y-git-main-sumanths-projects-952cfa2b.vercel.app",
+    "https://online-judge-fj7y-4q598qt6e-sumanths-projects-952cfa2b.vercel.app",
+];
 
 app.use(
     cors({
         origin: (origin, callback) => {
-            
             if (!origin || allowedOrigins.includes(origin)) {
                 callback(null, true);
             } else {
@@ -26,18 +29,12 @@ app.use(
             }
         },
         methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true, 
+        credentials: true,
         optionsSuccessStatus: 200,
     })
 );
 
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", allowedOrigins[0]); 
-    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-    res.header("Access-Control-Allow-Credentials", "true");
-    next();
-});
+
 
 
 app.use("/", router);
